751db86ce6a4a31b6175d5b3050efdc8b6cf9ff1
Jon Witts <jonwitts@users.noreply.github.com>
2019-09-25 22:03:32 +0100

    Update p2p-sender.sh

--- p2p-sender.sh
+++ p2p-sender.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/bash
+# add /bot/ssh
+# cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.bu
+# add /etc/wpa_supplicant/wpa_supplicant.conf
+# Change hostname to networks-p2p-sender
+# Set auto login on console as pi
+# reboot
+apt-get update && apt-get dist-upgrade -y
+apt-get autoremove -y
+apt-get install python3-pip
+pip3 install network
+# cp chat.py
