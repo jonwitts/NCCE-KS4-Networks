b9257f9c564ffcf1b5a27898a10a67295a2b48e0
Jon Witts <jonwitts@users.noreply.github.com>
2019-10-23 15:20:20 +0100

    Update dns-dhcp-server.sh

--- dns-dhcp-server.sh
+++ dns-dhcp-server.sh
@@ -24,13 +24,18 @@ apt-get update && apt-get dist-upgrade -y && apt-get autoremove -y && sudo apt-g
 #static ip_address=192.168.0.1/24
 #static routers=192.168.0.1
 #static domain_name_servers=192.168.0.1
-#
-service networking restart
 
 mv /etc/dnsmasq.conf /etc/dnsmasq.default
 nano /etc/dnsmasq.conf
 
 #interface=eth0
 #dhcp-range=192.168.0.50,192.168.0.254,255.255.255.0,12h
+#dhcp-option=6,192.168.0.1
+#no-hosts
+#addn-hosts=/etc/hosts.dnsmasq
+
+nano /etc/hosts.dnsmasq
+#192.168.0.1    dnsserver
 
 service dnsmasq restart
+service networking restart
