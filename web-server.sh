f4fb5c3bf6a875fe017adff51c224cd9008b45ec
Jon Witts <jonwitts@users.noreply.github.com>
2019-10-23 15:53:52 +0100

    Update web-server.sh

--- web-server.sh
+++ web-server.sh
@@ -21,7 +21,7 @@ apt-get update && apt-get dist-upgrade -y && apt-get autoremove -y && sudo apt-g
 #
 #interface eth0
 #
-#static ip_address=192.168.0.1/24
+#static ip_address=192.168.0.5/24
 #static routers=192.168.0.1
 #static domain_name_servers=192.168.0.1
 
